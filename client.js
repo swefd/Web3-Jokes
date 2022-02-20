7fba5aea3669284f91e900af907eede1ea88567f
swefd <swefd>
2022-02-20 13:50:59 +0200

    asd

--- client.js
+++ client.js
@@ -1,15 +1,9 @@
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'http://localhost:3000/jokes', true);
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8"');
 xhr.onload = function() {
     // do something to response
     console.log(this.responseText);
 };
 
-asd = {
-    "content": "asd haha smeshno",
-    "likes": 0,
-    "dislikes": 0
-}
-
-xhr.send(asd);
+xmlhttp.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }));
