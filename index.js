096f1b31cc437c38a2aec8ad0cc8cc5c5e1adf16
swefd <swefd>
2022-02-20 12:56:04 +0200

    Synch

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const server = http.createServer((req, res) => {
     if (req.url == '/jokes' && req.method == 'GET') {
         getAllJokes(req, res);
     }
+    if (req.url == '/jokes' && req.method == 'POST') {
+        addJoke(req, res);
+    }
 });
 server.listen(3000);
 
@@ -23,4 +26,27 @@ function getAllJokes(req, res) {
         allJokes.push(joke);
     }
     res.end(JSON.stringify(allJokes));
+}
+
+
+function addJoke(req, res) {
+    let data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        let joke = JSON.parse(data);
+        joke.likes = 0;
+        joke.dislikes = 0;
+
+        let dir = fs.readdirSync(dataPath);
+        let fileName = dir.length + '.json';
+        let filePath = path.join(dataPath, fileName);
+        fs.writeFileSync(filePath, JSON.stringify(joke));
+
+        res.end();
+    });
+
+
+
 }
